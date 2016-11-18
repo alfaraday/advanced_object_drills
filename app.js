bd1fdaafeba39f6d91ad9312d18664ed688bac9c
alfaraday <arianaloreleifaraday@gmail.com>
2016-11-18 09:00:32 -0500

    data stream merge

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
+//Most Frequent Word
 function mostFrequentWord(words) {
   var counter = {};
-  words.forEach(function(item) {
-    if (item in counter === true) {
-      counter.item += 1;
+  for (var i=0; i<=words.length; i++){
+    if (words[i] in counter) {
+      counter[words[i]] += 1;
     }
     else {
-      counter.item = 1;
+      counter[words[i]] = 1;
     }
-  });
-  var maxValue = 0
-  var maxKey = []
-  Object.keys(counter).forEach(function(key) {
-    if (counter.key > maxValue) {
-      maxValue = counter.key;
+  }
+  var maxKey = Object.keys(counter)[0];
+  var maxCount = counter[maxKey];
+  for (var word in counter) {
+    if (counter[word] > maxCount) {
+      maxKey = word;
+      maxCount  = counter[word];
     }
-  return maxValue;
-  });
-  Object.keys(counter).forEach(function(key) {
-    if (counter.key == maxValue) {
-      maxKey.push(key);
+  }
+  return maxKey;
+}
+
+//Data Merge
+function mergeDataStreams(stream1, stream2) {
+  if (stream1.id === stream2.id) {
+    var s2Keys = Object.keys(stream2);
+    for (var i=0; i<= s2Keys.length; i++) {
+      stream1[s2Keys[i]] = stream2[s2Keys[i]];
     }
-    return maxKey;
-  });
-  maxKey.sort();
-  return maxKey[0];
+  }
+  console.log(stream1);
+  return stream1;
 }
